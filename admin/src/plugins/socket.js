f53d73cb165da4c1cb2510575e9d8a5d52422183
Patrick <pkhadson@gmail.com>
2022-04-25 16:57:33 -0300

    a

--- admin/src/plugins/socket.js
+++ admin/src/plugins/socket.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
 import SocketIO from "socket.io-client";
 
-const connection = SocketIO("http://localhost:3000", {
-  //   channel: "order",
-  withCredentials: false,
-  reconnect: true,
-});
+const connection = SocketIO(
+  proccess.env.VUE_APP_API || "http://localhost:3000",
+  {
+    //   channel: "order",
+    withCredentials: false,
+    reconnect: true,
+  }
+);
 
 // connection.on("refreshList", console.log);
 // connection.emit("iamAdmin");
