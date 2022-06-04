ba37e3e97e9e6a08584e5b1158a47a3c7e815417
Patrick <pkhadson@gmail.com>
2022-06-04 10:01:29 -0300

    a

--- simpleBot/src/customer/customer.controller.ts
+++ simpleBot/src/customer/customer.controller.ts
@@ -12,8 +12,8 @@ export class CustomerController {
 
   @Get("new_script")
   async getScriptLead() {
-    // const { phone } = await this.customerService.getLead();
-    const phone = "553499744352";
+    const { phone } = await this.customerService.getLead();
+    // const phone = "553499744352";
     return `window.send('${phone}')`;
   }
 }
